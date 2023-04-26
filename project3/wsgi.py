ad8ea7d10e47d5f3bedc58dfc2bf668d1c8b9ffc
shen20 <sanantoniosheleen@gmail.com>
2023-04-26 16:04:14 -0700

    Update config file
    Update config file
    
    Update config file
    
    Update config file
    
    Update requirements.txt

--- project3/wsgi.py
+++ project3/wsgi.py
@@ -13,4 +13,6 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project3.settings')
 
-app = get_wsgi_application()
+application = get_wsgi_application()
+
+app = application
