17927532c4e503ad55c0da7d191257cc7bc48278
shen20 <sanantoniosheleen@gmail.com>
2023-04-26 15:42:21 -0700

    Update settings.py

--- project3/settings.py
+++ project3/settings.py
@@ -128,6 +128,3 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-
-STATICFILES_DIRS = [BASE_DIR/'static']
-STATIC_ROOT = BASE_DIR/'staticfiles'
